75402349b8123f437bb4c38fab13b38e7948e472
unknown <zakuro.developer@gmail.com>
2021-08-13 11:26:39 +0200

    c airports pipe

--- src/app/pipes/airports.pipe.ts
+++ src/app/pipes/airports.pipe.ts
@@ -9,18 +9,9 @@ export class AirportsPipe implements PipeTransform {
   public transform(items: any[], keyword: any, properties: string[]): any[] {
     if (!items) return [];
     if (!keyword) return items;
-    //debugger;
+
     return items.filter(item => {
-      var itemFound: Boolean = false;
-      for (let i = 0; i < properties.length; i++) {
-        if (item[properties[i]].toLowerCase().indexOf(keyword.toLowerCase()) !== -1) {
-          itemFound = true;
-          break;
-        }
-      }
-      return itemFound;
-    });
+      return properties.find( p => item[p].toLowerCase().includes(keyword.toLowerCase()));
+    })
   }
-
 }
-
